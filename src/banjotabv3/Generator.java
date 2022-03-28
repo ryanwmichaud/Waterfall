caf9921e969b1bd21530c8d6957e8ffa6b7a59db
ryanwmichaud <92121030+ryanwmichaud@users.noreply.github.com>
2022-03-28 16:55:00 -0400

    copied classes

---
+++ src/banjotabv3/Generator.java
@@ -0,0 +1,153 @@
+package banjotabv3;
+
+
+import java.util.ArrayList;
+
+
+public class Generator {
+	
+	int prevFret;
+	int prevSt;
+	
+	public enum NoteNames{
+		C,Db,D,Eb,E,F,Gb,G,Ab,A,Bb,B
+	}
+	
+	public Generator() {
+		prevSt=-100;
+		prevFret=0;
+	}
+	
+	/*
+		for(Integer fret: getChoices(sts,"E5")) {
+			System.out.println(String.valueOf(fret));
+		}
+	*/
+		
+	
+	public void printSts(ArrayList<St> sts) {
+		for(St s:sts) {
+			s.printSt();
+		}
+		System.out.println("\n");
+	}
+	public void printTab(ArrayList<St> sts) {
+		for(St s:sts) {
+			s.printStTab();
+		}
+	}
+	public void addBarline(ArrayList<St> sts) {
+		for(St s:sts) {
+			s.barline();
+		}
+	}
+	
+	public ArrayList<Integer> getChoices(ArrayList<St> sts,String note) {
+		ArrayList<Integer> arr = new ArrayList<Integer>();
+		for(St s:sts) {
+			arr.add(s.getFret(note));
+		}
+		return arr;
+	}
+	
+	public ArrayList<Integer> rank(ArrayList<Integer> choices,int prevSt,int prevFret) {
+		ArrayList<Integer> rankings= new ArrayList<Integer>();
+		//int pass=1;
+		for(int s=0;s<5;s++) { //as many passes as strings
+			//System.out.println("pass no. "+pass);
+			//pass++;
+			int choice = -100;
+			int iClosest = -100;
+			int smallestDist = 100;
+			int dist;
+			
+			for(int i=0;i<choices.size();i++) { //look at every string
+				
+				dist= choices.get(i)-prevFret;  //get the dist
+				if(dist<0) {
+					dist*=-1;
+				}
+				
+				//System.out.println("index is "+i+" fret is "+choices.get(i) +" dist to prev ("+prevFret+") is " + dist);
+		
+				if(prevSt!=i && choices.get(i)==0) {  //if its new string and open, done
+					iClosest=i;
+					break;
+				}
+				if(prevSt!=i && choices.get(i)!=-1 && dist<smallestDist) { //get closest on new string
+					smallestDist=dist;
+					iClosest = i;
+				}
+			}
+			
+			if(iClosest==-100) {  //if not found anywhere, choose prev string
+				iClosest=prevSt;  //it'll be a double up, or a rest "X" if it truly is not anywhere
+				break;
+			}
+		
+			choice = iClosest;
+			//System.out.println("we went with "+choices.get(choice)+" on index "+ choice+ ". dist of "+smallestDist+ " bc coming from "+prevSt);
+			/*
+			this.prevSt = choice;
+			if(choices.get(choice)!=0) {  //playing open strings doesn't require a shift
+				this.prevFret = choices.get(choice);
+			}
+			*/
+			//record best string and set it to -1. so we can get next best
+			rankings.add(choice);
+			choices.set(choice, -1);
+			//System.out.println(rankings.toString());
+		}
+		return rankings;
+	}
+	public int getPrevSt() {
+		return this.prevSt;
+	}
+	public void setPrevSt(int choice) {
+		this.prevSt = choice;
+		
+	}
+	public int getPrevFret() {
+		return this.prevFret;
+	}
+	public void setPrevFret(int choice) {
+		this.prevFret = choice;
+		
+	}
+	public void rest(ArrayList<St> sts) {
+		
+		for(St s:sts) {
+			s.rest();
+		}
+	}
+	public void delete(ArrayList<St> sts) {
+		for(St s:sts) {
+			s.delete();
+		}
+	}
+	
+	public void makeChoice(ArrayList<St> sts, ArrayList<Integer> choices, int strChoice) {
+		int i = 0;
+		for(St s:sts) {
+			//*****************************
+			
+			if(i==strChoice) {
+				s.choose(choices.get(i));
+			}else {
+				s.notChoose();
+			}
+			
+			//*****************************
+			/*
+			if(choices.get(i)!=-1) {
+				s.choose(choices.get(i));
+			}else {
+				s.notChoose();
+			}
+			*/
+			//******************************
+			i++;
+		}
+	}
+
+}
---
+++ src/banjotabv3/St.java
@@ -0,0 +1,161 @@
+package banjotabv3;
+
+
+import java.util.HashMap;
+
+import banjotabv3.Generator.NoteNames;
+
+
+public class St {
+	
+	protected HashMap<String,Integer> map = new HashMap<String,Integer>();  
+	String stringRep="";
+	int octave;
+	NoteNames open;
+	String tab = "";
+	int capo=-1;
+
+		
+	public St(NoteNames d, int octaveGiven) {
+		map.put(d+String.valueOf(octaveGiven), 0);
+		stringRep= stringRep.concat(d.toString()+octaveGiven+" ");
+		this.octave=octaveGiven;
+		this.open = d;
+		this.tab = this.tab.concat(this.open.toString()+" |");
+		buildFretBoard();
+		
+	}
+	
+	public St(NoteNames d, int octaveGiven, int capo) {
+		map.put(d+String.valueOf(octaveGiven), 0);
+		stringRep= stringRep.concat(d.toString()+octaveGiven+" ");
+		this.octave=octaveGiven;
+		this.open = d;
+		this.tab = this.tab.concat(this.open.toString()+" |");
+		this.capo=capo;
+		buildFretBoard();
+	}
+	
+	private void buildFretBoard() {
+		//build the rest of the fret board  
+				int fret = 1;
+				int currentOctave = this.octave;
+				int currentNoteNum = this.open.ordinal() + 1;   //change? to enum hashmap thing? 
+				
+				if(capo!=-1) {
+					for(int i =0;i<capo-1;i++) { 	//we want to leave orig string name at bottom if capo,dif from retuning. so put new "open" up the neck
+						if (i%2==0) {
+							stringRep= stringRep.concat("   ");
+						}else {
+							stringRep= stringRep.concat("    ");
+
+						}
+					}
+					currentNoteNum = this.open.ordinal()  + capo-5; 	//change starting note and fret
+					if (currentNoteNum>=12) {
+						currentNoteNum%=12;
+					}
+										
+					fret=capo;
+					
+					
+				}
+				
+				
+				
+				while(fret<22) {
+					if(currentNoteNum==12) {
+						currentNoteNum=0;
+						currentOctave++;
+					}
+					//Note current = new Note(NoteNames.values()[currentNoteNum],currentOctave);
+					
+					stringRep= stringRep.concat(NoteNames.values()[currentNoteNum]+String.valueOf(currentOctave)+" ");
+					
+					if(fret==capo) {
+						map.remove(this.open+String.valueOf(this.octave));
+						map.put(NoteNames.values()[currentNoteNum]+String.valueOf(currentOctave), 0);					
+					}else {
+						map.put(NoteNames.values()[currentNoteNum]+String.valueOf(currentOctave), fret);
+					}
+					fret++;
+					currentNoteNum++;
+				}
+			System.out.println(map.toString());	
+				
+	}
+	
+	/*
+private void buildFretBoardCapo()  {
+		
+		//build the rest of the fret board  
+		int fret = 1;
+		int currentNoteNum = this.open.ordinal()+1 ;   //change? to enum hashmap thing?
+		int currentOctave = this.octave;
+		
+		while(fret<=5) {	
+			fret++;
+		}
+		
+		while(fret<22) {
+			if(currentNoteNum==12) {
+				currentNoteNum=0;
+				currentOctave++;
+			}
+			//Note current = new Note(NoteNames.values()[currentNoteNum],currentOctave);
+			
+			stringRep= stringRep.concat(NoteNames.values()[currentNoteNum]+String.valueOf(currentOctave)+" ");
+			
+			map.put(NoteNames.values()[currentNoteNum]+String.valueOf(currentOctave), fret);
+			fret++;
+			currentNoteNum++;
+		}
+	}
+	*/
+	public void printSt(){
+		/*for(Note current:map.keySet()) {
+			System.out.printf("%d for %s\n",map.get(current), current.getName());
+		}*/
+		System.out.println(this.stringRep);
+	}
+	public void printStTab(){
+		System.out.println(this.tab);
+		this.tab=this.open.toString()+" |";
+	}
+	
+	public int getFret(String n) throws NullPointerException {
+		
+		int ret = -1;
+		try{
+			ret = map.get(n);
+		}catch(NullPointerException e) {
+			//System.out.printf("%s was not found on this string\n",n);
+		}
+		return ret;
+	}
+	public void rest() {
+		this.tab=this.tab.concat("X--");
+	}
+	public void delete() {
+		this.tab=this.tab.substring(0,this.tab.length()-3);
+	}
+	
+	public void choose(int fret) {
+		if(fret>9) { //check if single digit
+			this.tab=this.tab.concat(String.valueOf(fret)+"-");
+		}else if(fret==-1){
+			this.tab=this.tab.concat("X--");
+		}else {
+			this.tab=this.tab.concat(String.valueOf(fret)+"--");
+		}
+	}
+	public void barline() {
+		this.tab=this.tab.concat("|");
+	}
+	
+	public void notChoose() {
+		this.tab=this.tab.concat("---");
+	}
+	
+
+}
